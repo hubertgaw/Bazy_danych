0d7b04560fe865480dfd5b3a52791a3b960fbe03
KiszczixIsCoding <kamilkiszkozgierski@gmail.com>
2021-01-17 22:55:30 +0100

    Add some selects into zawieszenia, mecze, miasta

--- projekt/zapytania.sql
+++ projekt/zapytania.sql
@@ -89,3 +89,40 @@ WHERE pojemnosc_stadionu > (SELECT AVG(pojemnosc_stadionu)
 	AND k.id_ligi = l.id_ligi
 ORDER BY k.pojemnosc_stadionu DESC
 GO				
+
+--6. Wyświetlenie nazwy miasta, liczbę klubów z tego miasta (posortowane malejąco) oraz liczbę sędziów
+
+SELECT sel1.nazwa_miasta, sel1.ile_klubow, sel2.ile_sedziow  FROM
+	(SELECT m.id_miasta, m.nazwa_miasta, COUNT(k.id_klubu) as ile_klubow 
+	 FROM federacja.dbo.miasta m, federacja.dbo.kluby k
+		WHERE (m.id_miasta = k.id_miasta)
+		GROUP BY m.id_miasta, m.nazwa_miasta) as sel1,
+	(SELECT m.id_miasta, COUNT(s.id_sedziego) as ile_sedziow FROM federacja.dbo.miasta m
+		LEFT JOIN federacja.dbo.sedziowie s ON m.id_miasta = s.id_miasta
+		GROUP BY m.id_miasta, m.nazwa_miasta) as sel2
+		WHERE sel1.id_miasta = sel2.id_miasta
+		ORDER BY sel1.ile_klubow DESC, sel2.ile_sedziow DESC
+
+--7. Wyświetlenie liczby zwycięstw gospodarzy, remisów oraz zwycięstw gości w meczach ekstraklasy
+
+SELECT m1.zwyciestwa_gospodarzy, m0.remisy, m2.zwyciestwa_gosci FROM 	
+	(SELECT COUNT(*) AS zwyciestwa_gospodarzy FROM federacja.dbo.mecze m, federacja.dbo.kluby k
+		WHERE m.bramki#1 > m.bramki#2 and m.id_klubu#1 = k.id_klubu and k.id_ligi = 'E01') AS m1,
+	(SELECT COUNT(*) AS remisy FROM federacja.dbo.mecze m, federacja.dbo.kluby k
+		WHERE m.bramki#1 = m.bramki#2 and m.id_klubu#1 = k.id_klubu and k.id_ligi = 'E01') AS m0,
+	(SELECT COUNT(*) AS zwyciestwa_gosci FROM federacja.dbo.mecze m, federacja.dbo.kluby k
+		WHERE m.bramki#1 < m.bramki#2 and m.id_klubu#1 = k.id_klubu and k.id_ligi = 'E01') AS m2
+
+--8. Wyświetlenie imienia oraz nazwiska zawodnika, który przynajmniej raz był zawieszony. Poza tym,
+--  wyświetlana jest też nazwa klubu, nazwa ligi w której dany klub się znajduje oraz liczba zawieszeń 
+--  i liczba meczów, które obejmują zawieszenia. Sortowanie odbywa się na podstawie poziomu rozgrywkowego
+--  a w dalszej kolejności na podstawie liczby zawieszeń oraz ilości meczów, na ile został zawieszony zawodnik (malejąco) 
+
+SELECT  zawod.imie, zawod.nazwisko, k.nazwa AS klub, l.nazwa_ligi, 
+COUNT(*) AS ile_zawieszen, SUM(zawie.liczba_meczow) AS przez_ile_meczow_zawieszony 
+FROM federacja.dbo.kluby k, federacja.dbo.zawieszenia zawie, federacja.dbo.zawodnicy zawod, federacja.dbo.ligi l
+	WHERE zawie.id_zawodnika = zawod.id_zawodnika and zawod.id_klubu = k.id_klubu and k.id_ligi = l.id_ligi
+	GROUP BY zawod.imie, zawod.nazwisko, k.nazwa, l.poziom_rozgrywkowy, l.nazwa_ligi
+	ORDER BY l.poziom_rozgrywkowy, ile_zawieszen DESC, przez_ile_meczow_zawieszony DESC
+
+--9. 
